83f6f834fc1fc42aecfdb857ec189bb2f8219232
Hunter Dansin <hdansin@mailbox.org>
2019-11-18 20:45:52 -0500

    Templating and css with pug

--- app.js
+++ app.js
@@ -22,13 +22,19 @@ const port = process.env.PORT || "8000";
  *  App Configuration
  */
 
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
+app.use(express.static(path.join(__dirname, "public")));
+
+
 /**
  * Routes Definitions
  */
 
 
 app.get("/", (req, res) => {
-    res.status(200).send("Do It todo tracker.");
+    res.render("index", {title: "Home" });
 });
 
 
