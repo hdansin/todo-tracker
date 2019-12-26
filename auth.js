21dd82c05eb0d8b94808a3392a4ddada4a965528
Hunter Dansin <hdansin@mailbox.org>
2019-12-26 11:16:30 -0500

    changed redirect in auth.js callback line 36

--- auth.js
+++ auth.js
@@ -33,7 +33,7 @@ router.get("/callback", (req, res, next) => {
       return next(err);
     }
     if (!user) {
-      return res.redirect("/login");
+      return res.redirect("/user");
     }
     req.logIn(user, err => {
       if (err) {
@@ -41,7 +41,7 @@ router.get("/callback", (req, res, next) => {
       }
       const returnTo = req.session.returnTo;
       delete req.session.returnTo;
-      res.redirect(returnTo || "/user");
+      res.redirect(returnTo || "/");
     });
   })(req, res, next);
 });
