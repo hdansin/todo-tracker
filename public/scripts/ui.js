2657cf7873f87663a248fbcaeb8beb72562c038a
Hunter Dansin <hdansin@mailbox.org>
2020-02-26 21:28:33 -0500

    Got click to show task actions working

--- public/scripts/ui.js
+++ public/scripts/ui.js
@@ -1,18 +1,19 @@
 // UI Scripts
-function showTaskActions() {
+// eslint-disable-next-line no-unused-vars
+function showTaskActions(taskId) {
   // Get taskActions
-  let deleteTask = document.getElementById("deleteTask");
-  let duplicateTask = document.getElementById("duplicateTask");
-  let showEditForm = document.getElementById("showEditForm");
+  console.log(taskId);
+  let deleteTask = document.getElementById("deleteTask" + taskId);
+  let duplicateTask = document.getElementById("duplicateTask" + taskId);
+  let showEditForm = document.getElementById("showEditForm" + taskId);
   // Toggle visibility
-  if (deleteTask.style.visibility !== 'visible') {
-    deleteTask.style.visibility = 'visible';
-    duplicateTask.style.visibility = 'visible';
-    showEditForm.style.visibility = 'visible';
-  }
-  else {
-    deleteTask.style.visibility = 'hidden';
-    duplicateTask.style.visibility = 'hidden';
-    showEditForm.style.visibility = 'hidden';
+  if (deleteTask.style.visibility === "visible") {
+    deleteTask.style.visibility = "hidden";
+    duplicateTask.style.visibility = "hidden";
+    showEditForm.style.visibility = "hidden";
+  } else {
+    deleteTask.style.visibility = "visible";
+    duplicateTask.style.visibility = "visible";
+    showEditForm.style.visibility = "visible";
   }
 }
