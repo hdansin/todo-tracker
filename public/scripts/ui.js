65da17139ddfc5fa48609afbd9018bc132ddd236
hdansin <hdansin@mailbox.org>
2020-02-27 16:56:28 -0500

    Improved the task action click behavior with animation. Moved gloabl css to syle.scss instead of duplicating them in styleDark/styleLight

--- public/scripts/ui.js
+++ public/scripts/ui.js
@@ -2,7 +2,7 @@
 // eslint-disable-next-line no-unused-vars
 function showTaskActions(taskId) {
   // Get taskActions
-  console.log(taskId);
+  const showTaskActions = document.getElementById("showTaskActions" + taskId);
   let deleteTask = document.getElementById("deleteTask" + taskId);
   let duplicateTask = document.getElementById("duplicateTask" + taskId);
   let showEditForm = document.getElementById("showEditForm" + taskId);
@@ -11,9 +11,11 @@ function showTaskActions(taskId) {
     deleteTask.style.visibility = "hidden";
     duplicateTask.style.visibility = "hidden";
     showEditForm.style.visibility = "hidden";
+    showTaskActions.style.transform = "rotate(0deg)";
   } else {
     deleteTask.style.visibility = "visible";
     duplicateTask.style.visibility = "visible";
     showEditForm.style.visibility = "visible";
+    showTaskActions.style.transform = "rotate(90deg)";
   }
 }
